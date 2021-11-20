5528d5c411ebc1f56daf637ce89dd2984a3caaea
AlexanderAtanasov <bloodywavesofchaos@gmail.com>
2021-11-20 13:19:58 +0200

    Initial commit

---
+++ src/main/java/com/example/gameGuidesForUs/GameGuidesForUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.gameGuidesForUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameGuidesForUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameGuidesForUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gameGuidesForUs/GameGuidesForUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameGuidesForUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameGuidesForUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
