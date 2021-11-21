a02a67b06378e290a1d977657f7c49df3866a5d1
AlexanderAtanasov <bloodywavesofchaos@gmail.com>
2021-11-21 12:59:31 +0200

    Web Security + html/css test

--- src/main/java/com/example/gameGuidesForUs/config/ApplicationBeanConfiguration.java
+++ src/main/java/com/example/gameGuidesForUs/config/ApplicationBeanConfiguration.java
@@ -14,6 +14,7 @@ public class ApplicationBeanConfiguration {
         return new ModelMapper();
     }
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new Pbkdf2PasswordEncoder();
--- src/main/java/com/example/gameGuidesForUs/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/gameGuidesForUs/config/ApplicationSecurityConfiguration.java
@@ -1,10 +1,30 @@
 package com.example.gameGuidesForUs.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    //TODO add all security configurations
+    private final PasswordEncoder passwordEncoder;
+    private final UserDetailsService userDetailsService;
+
+    public ApplicationSecurityConfiguration(PasswordEncoder passwordEncoder, UserDetailsService userDetailsService) {
+        this.passwordEncoder = passwordEncoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                .antMatchers("/", "/users/login", "/users/register").permitAll();
+    }
 }
