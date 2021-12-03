24b2e18c3c8d413c435681301a3dd405f6b7cd46
AlexanderAtanasov <bloodywavesofchaos@gmail.com>
2021-12-03 16:51:46 +0200

    Fixed index page

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,31 +1,22 @@
 getGames()
 
 function getGames() {
-    $("#authors-container").empty();
+    $("#game-container").empty();
 
     fetch("http://localhost:8080/home")
         .then(response => response.json())
         .then(json => {
 
             if (json.screenshotUrl === null) {
-                $("#authors-container")
+                $("#game-container")
             } else {
             let srcRoute = json.screenshotUrl;
-            let tableCol = '<tc >' + '<td>' + json.gameTitle + '</td>' + '</tc>'
-            let tableRow = '<tr>' + '<td> + <img style="max-height: 50%; max-width: 50% " src="' + srcRoute + '"> + </td>' + '</tr>'
-            $("#authors-container").append(tableCol).append(tableRow)
+            let tableCol = '<tc class="bg-blur" style="font-size: 3rem" >' + '<td>' + json.gameTitle + '</td>' + '</tc>'
+            let tableRow = '<tr>' + '<td>  <img style="max-height: 50%; max-width: 50% " src="' + srcRoute + '">  </td>' + '</tr>'
+            $("#game-container").append(tableCol).append(tableRow)
         }
         })
 }
-
-// let tableRow =
-//     '<tr style="border: white solid">' +
-//     '<td>' + game.gameTitle + '</td>' +
-//     '<td>' + game.screenshotUrl + '</td>' +
-//     '<td>' +
-//     '<button class="edit-btn" data-book-id="' + game.id + '">View</button>' +
-//     '</td>' +
-//     '</tr>'
 
 
 $('body').on('click', 'button.delete-btn', function () {
