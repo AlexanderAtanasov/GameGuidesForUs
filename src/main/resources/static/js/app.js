b7710302881c264f048e337f6b2b1c6d401822ed
AlexanderAtanasov <bloodywavesofchaos@gmail.com>
2021-12-03 18:09:04 +0200

    Minor fixes

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -12,7 +12,7 @@ function getGames() {
             } else {
             let srcRoute = json.screenshotUrl;
             let tableCol = '<tc class="bg-blur" style="font-size: 3rem" >' + '<td>' + json.gameTitle + '</td>' + '</tc>'
-            let tableRow = '<tr>' + '<td>  <img style="max-height: 50%; max-width: 50% " src="' + srcRoute + '">  </td>' + '</tr>'
+            let tableRow = '<tr>' + '<td> <a href="/games/'+ json.gameId +'/view/"> <img style="max-height: 50%; max-width: 50% " src="' + srcRoute + '"> </a>  </td>' + '</tr>'
             $("#game-container").append(tableCol).append(tableRow)
         }
         })
